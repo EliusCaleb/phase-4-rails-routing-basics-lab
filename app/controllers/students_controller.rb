19c60d5f7083d20475dffca6b9ea7f5b81de4ded
EliusCaleb <eliuswanyoro@gmail.com>
2022-11-28 10:59:31 +0300

    Learning

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,17 @@
 class StudentsController < ApplicationController
     def index
-        @students = Student.all 
-        render json: @students
+        students = Student.all 
+        render json: students
     end 
 
     def grades 
-        @students = Student.order(grade: :desc)
-        render json: @students
+        students = Student.order(grade: :desc)
+        render json: students
     end
 
     def highest_grade
-        @students = Student.order(grade: :desc).first
+        students = Student.order(grade: :desc).first
 
-        render json: @students
+        render json: students
     end
 end
